8972b8729e97f5a18811c5cec63452553182a746
jygibson <jygibson922@gmail.com>
2018-11-27 07:49:45 -0500

    file structure

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon_cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("veggieburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("barbecue_burger", true);
